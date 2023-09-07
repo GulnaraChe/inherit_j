bc3afe1b2a7ba6993784233fbbb7855bb32ad340
Gulnara Che <g.chepurnova@gmail.com>
2023-09-07 20:07:52 +0500

    updt

---
+++ src/main/java/org/example/Task.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public abstract class Task {
+    protected int id;
+
+    public Task(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    public abstract boolean matches(String query);
+}
